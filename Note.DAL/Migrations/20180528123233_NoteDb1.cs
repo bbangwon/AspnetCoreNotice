e78259a74f5d987a2fb0c8ff1ba43fd79b420adb
breadone <bbangwon.chung@gamil.com>
2018-05-28 22:08:53 +0900

    엔터프라이즈 아키텍쳐 appsettins.json 을 이용한 ConnectionString 처리

---
+++ Note.DAL/Migrations/20180528123233_NoteDb1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Note.DAL.Migrations
+{
+    public partial class NoteDb1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notices",
+                columns: table => new
+                {
+                    NoticeNo = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    NoticeContents = table.Column<string>(nullable: false),
+                    NoticeTitle = table.Column<string>(nullable: false),
+                    NoticeWriteDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notices", x => x.NoticeNo);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notices");
+        }
+    }
+}
